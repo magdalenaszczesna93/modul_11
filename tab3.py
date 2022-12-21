77451f2de32d1cf32f977d3478509d3a00d55e3e
Magdalena Szczesna <magdalena.szczesna.ue@gmil.com>
2022-12-21 21:41:20 +0100

    modified tad3

--- tab3.py
+++ tab3.py
@@ -29,19 +29,3 @@ def render_tab(df):
                                 style={'display':'flex'}),
                         html.Div(id='temp-out')])
     return layout
-
-
-def render_tab(df):
-    layout = html.Div([html.H1('Sprzedaż globalna',
-                                style={'text-align':'center'}),
-                        html.Div([dcc.DatePickerRange(id='sales-range', 
-                                                    start_date=df['tran_date'].min(),
-                                                    end_date=df['tran_date'].max(), 
-                                                    display_format='YYYY-MM-DD')],
-                                style={'width':'100%','text-align':'center'}),
-                        html.Div([html.Div([dcc.Graph(id='bar-sales')],
-                                            style={'width':'50%'}),
-                                            html.Div([dcc.Graph(id='choropleth-sales')],
-                                                    style={'width':'50%'})],
-                                style={'display':'flex'})])
-    return layout
