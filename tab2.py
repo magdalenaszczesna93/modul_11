9340adfb3339b1dda8ab47daacec4452a3de7666
Magdalena Szczesna <magdalena.szczesna.ue@gmil.com>
2022-12-22 18:24:28 +0100

    modified

--- tab2.py
+++ tab2.py
@@ -8,7 +8,8 @@ def render_tab(df):
     fig = go.Figure(data=[go.Pie(labels=grouped.index,values=grouped.values)],layout=go.Layout(title='Udział grup produktów w sprzedaży'))
 
     layout = html.Div([html.H1('Produkty',style={'text-align':'center'}),
-                        html.Div([html.Div([dcc.Graph(id='pie-prod-cat',figure=fig)],style={'width':'50%'}),
+                        html.Div([html.Div([dcc.Graph(id='pie-prod-cat',figure=fig)],
+                                                        style={'width':'50%'}),
                                 html.Div([dcc.Dropdown(id='prod_dropdown', options=[{'label':prod_cat,
                                                         'value':prod_cat} for prod_cat in df['prod_cat'].unique()], 
                                                         value=df['prod_cat'].unique()[0]),
